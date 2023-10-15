eae28ad6eadb94e11bc3f656ab1fe81b09da3e52
Facuruku <facu.meza.lg@gmail.com>
2023-10-15 17:50:26 +0100

    header navigation icons instead of text

--- src/App.vue
+++ src/App.vue
@@ -9,30 +9,67 @@
 
       <div class="flex flex-grow items-center justify-end">
         <!-- Primary Navigation -->
-        <ul class="flex flex-row mt-1 text-xs">
+        <ul class="flex flex-row mt-1 gap-1">
           <!-- Navigation Links -->
           <li class="p-1 hover:scale-105">
-            <a
-              class="text-gray-300 border border-transparent rounded-full bg-neutral-950 bg-opacity-60 p-2"
-              href="#"
-              >Download app</a
-            >
+            <a class="text-white" href="#"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </a>
           </li>
 
           <li class="p-1 hover:scale-105">
-            <a
-              class="text-gray-300 border border-transparent rounded-full bg-neutral-950 bg-opacity-60 p-2"
-              href="#"
-              >Login / Register
-              <!--TODO change to icon  --></a
-            >
+            <a class="text-white rounded-full" href="#">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                />
+              </svg>
+            </a>
           </li>
           <li class="p-1 hover:scale-105">
-            <a
-              class="text-gray-300 border border-transparent rounded-full bg-neutral-950 bg-opacity-60 p-2"
-              href="#"
-              >Manage</a
-            >
+            <a class="text-white" href="#"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
+                />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+              </svg>
+            </a>
           </li>
         </ul>
       </div>
@@ -90,7 +127,7 @@
       <!-- Playlist -->
       <ol id="playlist">
         <li
-          class="flex justify-between items-center p-2 pl-6 cursor-pointer transition duration-800 hover:bg-neutral-800"
+          class="flex justify-between items-center p-2 pl-6 cursor-pointer transition duration-800 rounded-md hover:bg-neutral-800"
         >
           <div>
             <a href="#" class="font-bold block text-gray-100">Song Title</a>
@@ -105,7 +142,7 @@
           </div>
         </li>
         <li
-          class="flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-neutral-800"
+          class="flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 rounded-md hover:bg-neutral-800"
         >
           <div>
             <a href="#" class="font-bold block text-gray-100">Song Title</a>
@@ -305,7 +342,7 @@
         <!-- Current Position -->
         <div class="player-currenttime text-xs secondary-text">00:00</div>
         <!-- Scrub Container  -->
-        <div class="group w-80 h-1 rounded relative bg-neutral-700 cursor-pointer">
+        <div class="group w-80 h-1 rounded bg-neutral-700 cursor-pointer">
           <!-- Player Ball -->
           <span
             class="absolute -top-1.5 -ml-2.5 text-white text-xs opacity-0 group-hover:opacity-100"
