b85b8e5fc903d094fbe7ec1a11b3417574bbb28a
Facuruku <facu.meza.lg@gmail.com>
2023-10-26 16:41:33 +0100

    Feature: display songs in playlist WIP

--- src/components/AppPlaylist.vue
+++ src/components/AppPlaylist.vue
@@ -1,285 +1,104 @@
 <template>
-  <div class="relative min-h-screen">
-    <section
-      class="absolute inset-0 min-h-screen bg-gradient-to-t from-zinc-900 from-60% to-[#4a368e] to-90% w-full"
-    ></section>
-    <!-- Playlist header -->
-    <section
-      class="relative z-10 mx-10 pt-40 pb-6 text-white text-center relative font-circular-regular"
-    >
-      <div class="container flex gap-6">
-        <img
-          aria-hidden="false"
-          draggable="false"
-          loading="eager"
-          src="/assets/img/liked-songs-300.webp"
-          alt="Liked songs"
-          class="shadow-3xl"
-          srcset="
-            /assets/img/liked-songs-300.webp 150w,
-            /assets/img/liked-songs-300.webp 300w,
-            /assets/img/liked-songs-640.webp 320w,
-            /assets/img/liked-songs-640.webp 640w
-          "
-          sizes="(min-width: 1280px) 232px, 192px"
+  <!-- Playlist header -->
+  <section class="z-10 mx-10 pt-40 pb-6 text-white text-center relative font-circular-regular">
+    <div class="container flex gap-6">
+      <img
+        aria-hidden="false"
+        draggable="false"
+        loading="eager"
+        src="/assets/img/liked-songs-300.webp"
+        alt="Liked songs"
+        class="shadow-3xl"
+        srcset="
+          /assets/img/liked-songs-300.webp 150w,
+          /assets/img/liked-songs-300.webp 300w,
+          /assets/img/liked-songs-640.webp 320w,
+          /assets/img/liked-songs-640.webp 640w
+        "
+        sizes="(min-width: 1280px) 232px, 192px"
+      />
+      <div
+        class="text-white main-header-content flex flex-col grow justify-end items-start content-normal"
+      >
+        <h4 class="text-sm mb-2">Playlist</h4>
+        <h1 class="text-5xl mb-5 font-circular-black tracking-tighter">Liked songs</h1>
+        <p class="">
+          <span>{{ getUserName }} º {{ songs.length }} songs </span>
+        </p>
+      </div>
+    </div>
+  </section>
+  <!-- Playlist Content -->
+  <section
+    class="relative z-10 px-10 py-5 mb-20 bg-zinc-900 bg-opacity-30 font-circular-regular min-h-[50vh]"
+  >
+    <button type="button">
+      <p
+        class="text-[#1ed760] text-5xl hover:scale-110 bg-black rounded-full"
+        :class="{ 'fa fa-play-circle': !isPlaying, 'fa fa-pause-circle': isPlaying }"
+        @click.prevent="isPlaying ? pause() : play()"
+      ></p>
+    </button>
+
+    <!-- Playlist -->
+
+    <table id="playlist" class="mx-6 mt-6 secondary-text w-full text-left px-6 pt-6 pb-2">
+      <thead class="border-b border-zinc-300 border-opacity-10">
+        <tr>
+          <th class=""><span class="font-sans">#</span></th>
+          <th>Title</th>
+          <th>Genre</th>
+          <th>Comments</th>
+          <th class="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="h-6 secondary-text"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </th>
+        </tr>
+      </thead>
+
+      <tbody class="text-white mb-10">
+        <div v-if="songs.length === 0">Empty playlist.</div>
+        <SongItem
+          v-else
+          v-for="(song, index) in songs"
+          :key="song.docID"
+          :song="song"
+          :index="index"
         />
-        <div
-          class="text-white main-header-content flex flex-col grow justify-end items-start content-normal"
-        >
-          <h4 class="text-sm mb-2">Playlist</h4>
-          <h1 class="text-5xl mb-5 font-circular-black tracking-tighter">Liked songs</h1>
-          <p class="">
-            <span>{{ getUserName }} º {{ getPlaylistSongsSize }} songs </span>
-          </p>
-        </div>
-      </div>
-    </section>
-    <!-- Playlist Content -->
-    <section class="relative z-10 px-10 py-5 mb-20 bg-black bg-opacity-30 font-circular-regular">
-      <button type="button">
-        <p
-          class="text-[#1ed760] text-5xl hover:scale-110 bg-black rounded-full"
-          :class="{ 'fa fa-play-circle': !isPlaying, 'fa fa-pause-circle': isPlaying }"
-          @click.prevent="isPlaying ? pause() : play()"
-        ></p>
-      </button>
-      <div class="flex flex-col">
-        <div class="px-6 pt-6 pb-2 font-bold border-b border-zinc-300 border-opacity-10">
-          <span class="card-title secondary-text">Title</span>
-          <!--TODO add columns like spotify <span class="card-title secondary-text">Album</span> -->
-          <!-- Maybe refactor to use table -->
-
-          <!-- Icon -->
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6 float-right secondary-text"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-        </div>
-        <!-- Playlist -->
-        <ol id="playlist" class="font-circular-thin text-white mb-10">
-          <li
-            class="flex justify-between items-center p-2 cursor-pointer transition duration-800 rounded-md hover:bg-white hover:bg-opacity-10"
-          >
-            <div>
-              <a href="#" class="font-bold block">Song Title</a>
-              <span class="secondary-text text-sm">Artist Name</span>
-            </div>
-
-            <div class="text-lg secondary-text">
-              <span class="comments">
-                <i class="fa fa-comments"></i>
-                15
-              </span>
-            </div>
-          </li>
-          <li
-            class="flex justify-between items-center p-2 cursor-pointer transition duration-300 rounded-md hover:bg-white hover:bg-opacity-10"
-          >
-            <div>
-              <a href="#" class="font-bold block">Song Title</a>
-              <span class="secondary-text text-sm">Artist Name</span>
-            </div>
-
-            <div class="text-lg">
-              <span class="comments">
-                <i class="fa-solid fa-comments"></i>
-                15
-              </span>
-            </div>
-          </li>
-          <li
-            class="flex justify-between items-center p-2 cursor-pointer transition duration-300 hover:bg-white hover:bg-opacity-10"
-          >
-            <div>
-              <a href="#" class="font-bold block">Song Title</a>
-              <span class="text-sm">Artist Name</span>
-            </div>
-
-            <div class="text-lg">
-              <span class="comments">
-                <i class="fa fa-comments"></i>
-                15
-              </span>
-            </div>
-          </li>
-          <li
-            class="flex justify-between items-center p-2 cursor-pointer transition duration-300 hover:bg-white hover:bg-opacity-10"
-          >
-            <div>
-              <a href="#" class="font-bold block">Song Title</a>
-              <span class="text-sm">Artist Name</span>
-            </div>
-
-            <div class="text-lg">
-              <span class="comments">
-                <i class="fa fa-comments"></i>
-                15
-              </span>
-            </div>
-          </li>
-          <li
-            class="flex justify-between items-center p-2 cursor-pointer transition duration-300 hover:bg-white hover:bg-opacity-10"
-          >
-            <div>
-              <a href="#" class="font-bold block">Song Title</a>
-              <span class="text-sm">Artist Name</span>
-            </div>
-
-            <div class="text-lg">
-              <span class="comments">
-                <i class="fa fa-comments"></i>
-                15
-              </span>
-            </div>
-          </li>
-          <li
-            class="flex justify-between items-center p-2 cursor-pointer transition duration-300 hover:bg-white hover:bg-opacity-10"
-          >
-            <div>
-              <a href="#" class="font-bold block">Song Title</a>
-              <span class="text-sm">Artist Name</span>
-            </div>
-
-            <div class="text-lg">
-              <span class="comments">
-                <i class="fa fa-comments"></i>
-                15
-              </span>
-            </div>
-          </li>
-          <li
-            class="flex justify-between items-center p-2 cursor-pointer transition duration-300 hover:bg-white hover:bg-opacity-10"
-          >
-            <div>
-              <a href="#" class="font-bold block">Song Title</a>
-              <span class="text-sm">Artist Name</span>
-            </div>
-
-            <div class="text-lg">
-              <span class="comments">
-                <i class="fa fa-comments"></i>
-                15
-              </span>
-            </div>
-          </li>
-          <li
-            class="flex justify-between items-center p-2 cursor-pointer transition duration-300 hover:bg-white hover:bg-opacity-10"
-          >
-            <div>
-              <a href="#" class="font-bold block">Song Title</a>
-              <span class="text-sm">Artist Name</span>
-            </div>
-
-            <div class="text-lg">
-              <span class="comments">
-                <i class="fa fa-comments"></i>
-                15
-              </span>
-            </div>
-          </li>
-          <li
-            class="flex justify-between items-center p-2 cursor-pointer transition duration-300 hover:bg-white hover:bg-opacity-10"
-          >
-            <div>
-              <a href="#" class="font-bold block">Song Title</a>
-              <span class="text-sm">Artist Name</span>
-            </div>
-
-            <div class="text-lg">
-              <span class="comments">
-                <i class="fa fa-comments"></i>
-                15
-              </span>
-            </div>
-          </li>
-          <li
-            class="flex justify-between items-center p-2 cursor-pointer transition duration-300 hover:bg-white hover:bg-opacity-10"
-          >
-            <div>
-              <a href="#" class="font-bold block">Song Title</a>
-              <span class="text-sm">Artist Name</span>
-            </div>
-
-            <div class="text-lg">
-              <span class="comments">
-                <i class="fa fa-comments"></i>
-                15
-              </span>
-            </div>
-          </li>
-          <li
-            class="flex justify-between items-center p-2 cursor-pointer transition duration-300 hover:bg-white hover:bg-opacity-10"
-          >
-            <div>
-              <a href="#" class="font-bold block">Song Title</a>
-              <span class="text-sm">Artist Name</span>
-            </div>
-
-            <div class="text-lg">
-              <span class="comments">
-                <i class="fa fa-comments"></i>
-                15
-              </span>
-            </div>
-          </li>
-          <li
-            class="flex justify-between items-center p-2 cursor-pointer transition duration-300 hover:bg-gray-50"
-          >
-            <div>
-              <a href="#" class="font-bold block">Song Title</a>
-              <span class="text-sm">Artist Name</span>
-            </div>
-
-            <div class="text-lg">
-              <span class="comments">
-                <i class="fa fa-comments"></i>
-                15
-              </span>
-            </div>
-          </li>
-          <li
-            class="flex justify-between items-center p-2 cursor-pointer transition duration-300 hover:bg-gray-50 hover:bg-opacity-0"
-          >
-            <div>
-              <a href="#" class="font-bold block">Song Title</a>
-              <span class="text-sm">Artist Name</span>
-            </div>
-
-            <div class="text-lg">
-              <span class="comments">
-                <i class="fa fa-comments"></i>
-                15
-              </span>
-            </div>
-          </li>
-        </ol>
-        <!-- .. end Playlist -->
-      </div>
-    </section>
-  </div>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script>
 import { auth } from '@/includes/firebase'
 import { mapState, mapActions } from 'pinia'
 import usePlayerStore from '@/stores/player'
+import SongItem from '@/components/SongItem.vue'
 
 export default {
   name: 'AppPlaylist',
-  data() {
-    return {
-      songsInPlaylist: auth.currentUser ? 6 : 0
+  components: { SongItem },
+  props: {
+    songs: {
+      type: Array,
+      required: true
     }
+  },
+  data() {
+    return {}
   },
   computed: {
     ...mapState(usePlayerStore, ['isPlaying']),
@@ -287,12 +106,6 @@ export default {
       return auth.currentUser && auth.currentUser.displayName
         ? auth.currentUser.displayName
         : 'Your name here'
-    },
-    getPlaylistSongs() {
-      return [{ name: 'Perfect', artist: 'Ed Sheeran' }]
-    },
-    getPlaylistSongsSize() {
-      return 6
     }
   },
   methods: {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- <section class="bg-gradient-to-t from-zinc-900 from-60% to-[#4a368e] to-90%"> -->
   <section>
-    <AppPlaylist />
+    <AppPlaylist :songs="songs" />
 
     <AppPlayer />
   </section>
@@ -10,8 +10,24 @@
 <script>
 import AppPlaylist from '@/components/AppPlaylist.vue'
 import AppPlayer from '@/components/AppPlayer.vue'
+import { songsCollection } from '@/includes/firebase'
 
 export default {
-  components: { AppPlaylist, AppPlayer }
+  name: 'Home',
+  components: { AppPlaylist, AppPlayer },
+  data() {
+    return {
+      songs: []
+    }
+  },
+  async created() {
+    const snapshots = await songsCollection.get()
+    snapshots.forEach((document) => {
+      this.songs.push({
+        docID: document.id,
+        ...document.data()
+      })
+    })
+  }
 }
 </script>
