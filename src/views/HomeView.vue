3cd5a4bff57a3aec621f3e011921fdba4f8303ec
Facuruku <facu.meza.lg@gmail.com>
2023-10-26 18:07:38 +0100

    Feature: creata infinite scrolling songs (lazy loading)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,17 +17,62 @@ export default {
   components: { AppPlaylist, AppPlayer },
   data() {
     return {
-      songs: []
+      songs: [],
+      maxPerPage: 10,
+      pendingReq: false
     }
   },
   async created() {
-    const snapshots = await songsCollection.get()
-    snapshots.forEach((document) => {
-      this.songs.push({
-        docID: document.id,
-        ...document.data()
-      })
-    })
+    await this.getSongs()
+
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    async getSongs() {
+      if (this.pendingReq) {
+        return
+      }
+
+      this.pendingReq = true
+
+      let snapshots
+
+      if (this.songs.length) {
+        const lastDoc = await songsCollection.doc(this.songs[this.songs.length - 1].docID).get()
+        snapshots = await songsCollection
+          .orderBy('modified_name')
+          .startAfter(lastDoc)
+          .limit(this.maxPerPage)
+          .get()
+        snapshots.forEach((document) => {
+          this.songs.push({
+            docID: document.id,
+            ...document.data()
+          })
+        })
+      } else {
+        snapshots = await songsCollection.orderBy('modified_name').limit(this.maxPerPage).get()
+        snapshots.forEach((document) => {
+          this.songs.push({
+            docID: document.id,
+            ...document.data()
+          })
+        })
+      }
+      this.pendingReq = false
+    },
+    handleScroll() {
+      const { scrollTop, offsetHeight } = document.documentElement
+      const { innerHeight } = window
+      const bottomOfWindow = Math.ceil(scrollTop) + innerHeight >= offsetHeight
+
+      if (bottomOfWindow) {
+        this.getSongs()
+      }
+    }
+  },
+  beforeUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
   }
 }
 </script>
