5b95b997a8fc5e3fd25bbacdf53fec5be9285bd0
Facuruku <facu.meza.lg@gmail.com>
2023-11-27 19:26:37 +0000

    Style: add icon to home link

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -3,8 +3,7 @@
     <div class="flex flex-col gap-5 justify-center items-center pt-32">
       <h1>Melody not found</h1>
       <h4>We didn't found the page you are looking for</h4>
-
-      <RouterLink :to="{ name: 'home' }">Take me home!</RouterLink>
+      <RouterLink :to="{ name: 'home' }">Take me home! <i v-icon="'house'"></i></RouterLink>
     </div>
   </main>
 </template>
