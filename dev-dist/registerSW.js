b1262ce59eaa559327e5b3597ab2852b8146fcda
Facuruku <facu.meza.lg@gmail.com>
2024-01-13 16:57:04 +0000

    Feature: add Vite PWA plugin and update manifest basic configuration

---
+++ dev-dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/musicfy/dev-sw.js?dev-sw', { scope: '/musicfy/', type: 'classic' })
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,53 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/musicfy/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Musicfy',
+        short_name: 'Musicfy',
+        description: 'Listen to your favorite music anywhere',
+        theme_color: '#18181b',
+        background_color: '#000000',
+        icons: [
+          {
+            src: 'assets/img/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'assets/img/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+        screenshots: [
+          {
+            src: 'assets/img/screenshot-wide.png',
+            sizes: '1920x1080',
+            type: 'image/png',
+            form_factor: 'wide'
+          },
+          {
+            src: 'assets/img/screenshot-mobile.png',
+            sizes: '396x843',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
