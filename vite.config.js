1a6df8920941f57881bacf63aaeeeec23ac39df6
Facuruku <facu.meza.lg@gmail.com>
2024-01-16 00:17:57 +0000

    Deactivate rollup-plugin-visualizer

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
-import { visualizer } from 'rollup-plugin-visualizer'
+//import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
