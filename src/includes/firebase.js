14a0b23a747eb190ca9fa48b7389bbfd9130ec19
Facuruku <facu.meza.lg@gmail.com>
2024-05-25 20:30:33 +0100

    Update Firebase configuration to use environment variables

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -6,13 +6,13 @@ import 'firebase/storage'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyARoJhMNEPSXdWy5m1-CNhA6lvkRApVg0o',
-  authDomain: 'musicfy-53b66.firebaseapp.com',
-  projectId: 'musicfy-53b66',
-  storageBucket: 'musicfy-53b66.appspot.com',
-  //messagingSenderId: '349325047458',
-  appId: '1:349325047458:web:98dca1dbd405cc352bc9fb'
-  //measurementId: 'G-JKG974JFM4'
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  //messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 }
 
 // Initialize Firebase
