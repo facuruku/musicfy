65bf77c306bc9c58240c323de0b10860a3054879
Facuruku <facu.meza.lg@gmail.com>
2024-05-25 19:43:20 +0100

    Vitest initial config + sanity test

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true
+  },
   //base: '/musicfy/',
   plugins: [
     vue(),
